db6e9adda0c3b36c152295b83d74594bc09435ed
PhitzZ <selasorxpxp1548@gmail.com>
2024-07-10 00:55:13 +0800

    client-uwu.py

--- client-uwu.py
+++ client-uwu.py
@@ -1,14 +1,22 @@
 import socket
 import threading
 import sys
+import random
+from colorama import init, Fore
 
-def receive_messages(client_socket):
+# Initialize colorama
+init()
+
+# List of available colors
+COLORS = [Fore.RED, Fore.GREEN, Fore.YELLOW, Fore.BLUE, Fore.MAGENTA, Fore.CYAN, Fore.WHITE]
+
+def receive_messages(client_socket, color):
     while True:
         try:
             message = client_socket.recv(1024).decode('utf-8')
             if not message:
                 break
-            print(message)
+            print(color + message + Fore.RESET)
         except:
             print("Connection closed.")
             client_socket.close()
@@ -18,10 +26,13 @@ def start_client(host, port):
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect((host, port))
 
-    receive_thread = threading.Thread(target=receive_messages, args=(client,))
+    # Assign a random color to this client
+    color = random.choice(COLORS)
+
+    receive_thread = threading.Thread(target=receive_messages, args=(client, color))
     receive_thread.start()
 
-    print("Connected to the chat server. Type your messages below:")
+    print("Connected to the chatUwu. \nType your UwU messages below:")
     while True:
         message = input()
         if message.lower() == 'exit':
